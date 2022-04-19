3978b32d523c56d6db17e2f7ce35fab917da0caa
Олег Трушков <oleg_trushkov@hotmail.com>
2022-04-19 13:45:50 +0300

    Add project files.

---
+++ VS2019.GlowWindow/Data/GlowWindow/ChangeScope.cs
@@ -0,0 +1,19 @@
+﻿namespace VS2019.GlowWindow.Data
+{
+    internal class ChangeScope : DisposableObject
+    {
+        private readonly Controls.GlowWindow _window;
+
+        public ChangeScope(Controls.GlowWindow window)
+        {
+            _window = window;
+            _window.DeferGlowChangesCount++;
+        }
+
+        protected override void DisposeManagedResources()
+        {
+            _window.DeferGlowChangesCount--;
+            if (_window.DeferGlowChangesCount == 0) _window.EndDeferGlowChanges();
+        }
+    }
+}
---
+++ VS2019.GlowWindow/Data/GlowWindow/GlowBitmapPart.cs
@@ -0,0 +1,22 @@
+﻿namespace VS2019.GlowWindow.Data
+{
+    internal enum GlowBitmapPart
+    {
+        CornerTopLeft,
+        CornerTopRight,
+        CornerBottomLeft,
+        CornerBottomRight,
+        TopLeft,
+        Top,
+        TopRight,
+        LeftTop,
+        Left,
+        LeftBottom,
+        BottomLeft,
+        Bottom,
+        BottomRight,
+        RightTop,
+        Right,
+        RightBottom
+    }
+}
