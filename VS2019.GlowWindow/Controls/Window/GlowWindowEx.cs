a9f636c5415e4fdd2fec6b43744d501da0d8e266
Олег Трушков <oleg_trushkov@hotmail.com>
2022-05-06 12:54:55 +0300

    Update deps. Fix icon mouse down logic.

--- VS2019.GlowWindow/Controls/Window/GlowWindowEx.cs
+++ VS2019.GlowWindow/Controls/Window/GlowWindowEx.cs
@@ -3,10 +3,11 @@ using System.Reflection;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Interop;
+using System.Windows.Media;
 using Windows.Win32;
+using Windows.Win32.Foundation;
 using Windows.Win32.UI.Controls;
 using Windows.Win32.UI.WindowsAndMessaging;
-using Windows.Win32.Foundation;
 
 namespace VS2019.GlowWindow.Controls
 {
@@ -14,18 +15,18 @@ namespace VS2019.GlowWindow.Controls
     {
         #region CustomWindowChrome
 
-        private static readonly PropertyInfo? CriticalHandlePropertyInfo = typeof(Window).GetProperty("CriticalHandle", BindingFlags.NonPublic | BindingFlags.Instance);
-        private static readonly object[] EmptyObjectArray = Array.Empty<object>();
-
         private const SET_WINDOW_POS_FLAGS SwpFlags = SET_WINDOW_POS_FLAGS.SWP_FRAMECHANGED | SET_WINDOW_POS_FLAGS.SWP_NOSIZE | SET_WINDOW_POS_FLAGS.SWP_NOMOVE | SET_WINDOW_POS_FLAGS.SWP_NOZORDER | SET_WINDOW_POS_FLAGS.SWP_NOOWNERZORDER | SET_WINDOW_POS_FLAGS.SWP_NOACTIVATE;
+
+        private static readonly PropertyInfo? _criticalHandlePropertyInfo = typeof(Window).GetProperty("CriticalHandle", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly object[] _emptyObjectArray = Array.Empty<object>();
 
         protected override void OnSourceInitialized(EventArgs e)
         {
             base.OnSourceInitialized(e);
             if (PresentationSource.FromDependencyObject(this) is not HwndSource hWnd) return;
             hWnd.AddHook(WndProc);
-            if (hWnd.CompositionTarget != null)
-                hWnd.CompositionTarget.BackgroundColor = System.Windows.Media.Colors.Transparent;
+            if (hWnd.CompositionTarget is not null)
+                hWnd.CompositionTarget.BackgroundColor = Colors.Transparent;
             MARGINS dwmMargin = new();
             PInvoke.DwmExtendFrameIntoClientArea((HWND)hWnd.Handle, in dwmMargin);
             PInvoke.SetWindowPos((HWND)hWnd.Handle, (HWND)IntPtr.Zero, 0, 0, 0, 0, SwpFlags);
@@ -50,41 +51,35 @@ namespace VS2019.GlowWindow.Controls
             switch (e.ClickCount)
             {
                 case 1:
+                    e.Handled = true;
+
+                    VerifyAccess();
+                    PInvoke.ReleaseCapture();
+
+                    if (_criticalHandlePropertyInfo is not null)
                     {
-                        e.Handled = true;
-
-                        VerifyAccess();
-                        _ = PInvoke.ReleaseCapture();
-
-                        if (CriticalHandlePropertyInfo != null)
+                        var criticalHandle = _criticalHandlePropertyInfo.GetValue(this, _emptyObjectArray);
+                        var wpfPoint = PointToScreen(Mouse.GetPosition(this));
+                        var x = (int)wpfPoint.X;
+                        var y = (int)wpfPoint.Y;
+                        if (criticalHandle is not null)
                         {
-                            var criticalHandle = CriticalHandlePropertyInfo.GetValue(this, EmptyObjectArray);
-                            var wpfPoint = PointToScreen(Mouse.GetPosition(this));
-                            var x = (int)wpfPoint.X;
-                            var y = (int)wpfPoint.Y;
-                            if (criticalHandle != null)
-                            {
-                                _ = PInvoke.SendMessage((HWND)(IntPtr)criticalHandle, PInvoke.WM_NCLBUTTONDOWN, (UIntPtr)PInvoke.HTCAPTION, new IntPtr(x | (y << 16)));
-                            }
+                            PInvoke.SendMessage((HWND)(IntPtr)criticalHandle, PInvoke.WM_NCLBUTTONDOWN, (UIntPtr)PInvoke.HTCAPTION, new IntPtr(x | (y << 16)));
                         }
-
-                        break;
                     }
+                    break;
                 case 2 when ResizeMode != ResizeMode.NoResize:
+                    e.Handled = true;
+
+                    if (WindowState == WindowState.Normal && ResizeMode != ResizeMode.NoResize && ResizeMode != ResizeMode.CanMinimize)
                     {
-                        e.Handled = true;
-
-                        if (WindowState == WindowState.Normal && ResizeMode != ResizeMode.NoResize && ResizeMode != ResizeMode.CanMinimize)
-                        {
-                            SystemCommands.MaximizeWindow(this);
-                        }
-                        else
-                        {
-                            SystemCommands.RestoreWindow(this);
-                        }
-
-                        break;
+                        SystemCommands.MaximizeWindow(this);
                     }
+                    else
+                    {
+                        SystemCommands.RestoreWindow(this);
+                    }
+                    break;
             }
         }
 
@@ -99,15 +94,12 @@ namespace VS2019.GlowWindow.Controls
             switch (e.ClickCount)
             {
                 case 1:
+                    if (sender is FrameworkElement icon)
                     {
-                        if (sender is FrameworkElement icon)
-                        {
-                            var point = icon.TransformToAncestor(this).Transform(new Point(0, icon.ActualHeight));
-                            SystemCommands.ShowSystemMenu(this, point);
-                        }
-
-                        break;
+                        var point = PointToScreen(icon.TransformToAncestor(this).Transform(new Point(0, icon.ActualHeight)));
+                        SystemCommands.ShowSystemMenu(this, point);
                     }
+                    break;
                 case 2:
                     Close();
                     break;
@@ -120,11 +112,10 @@ namespace VS2019.GlowWindow.Controls
 
         protected override void OnInitialized(EventArgs e)
         {
-            _ = CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, OnCloseWindow));
-            _ = CommandBindings.Add(new CommandBinding(SystemCommands.MaximizeWindowCommand, OnMaximizeWindow, OnCanResizeWindow));
-            _ = CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand, OnMinimizeWindow, OnCanMinimizeWindow));
-            _ = CommandBindings.Add(new CommandBinding(SystemCommands.RestoreWindowCommand, OnRestoreWindow, OnCanResizeWindow));
-
+            CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, OnCloseWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.MaximizeWindowCommand, OnMaximizeWindow, OnCanResizeWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand, OnMinimizeWindow, OnCanMinimizeWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.RestoreWindowCommand, OnRestoreWindow, OnCanResizeWindow));
             base.OnInitialized(e);
         }
 
